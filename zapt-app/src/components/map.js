52b794a867c3b2b51740c79633a1b822122b79a2
Kusko99 <fernandaofo120@gmail.com>
2024-12-02 10:27:08 -0300

    Atualizando para utilizar o react-dotenv

--- zapt-app/src/components/map.js
+++ zapt-app/src/components/map.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import env from "react-dotenv";
 
 export default class Map extends Component {
     render() {
-        const placeId = process.env.REACT_APP_PLACE_ID;
-        const apiKey = process.env.REACT_APP_API_KEY;
+        const placeId = env.PLACE_ID;
+        const apiKey = env.API_KEY;
         const iframeSrc = `https://app.zapt.tech/#/map?placeId=${placeId}&embed=true&apiKey=${apiKey}`;
         return (
             <div>
                 <iframe src={iframeSrc} title="Map" width="100%" height="600"></iframe>
             </div>
-        )
+        );
     }
 }
